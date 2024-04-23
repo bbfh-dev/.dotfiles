261870cfff47a0a72b784482acbb81c1e5f12ff1
BubbleFish <daforsastudia@gmail.com>
2024-04-23 22:19:39 +0300

    Update to 23.04.2024

--- scripts/sessionizer.sh
+++ scripts/sessionizer.sh
@@ -10,8 +10,19 @@ if [[ $selected == *.d ]]; then
 	selected=$(find $selected -mindepth 1 -maxdepth 1 -type d,l | fzf)
 fi
 
-if [[ $selected != */bookmarks/LATEST ]]; then
-	rm -rf ~/public/bookmarks/LATEST
-	ln -s $selected ~/public/bookmarks/LATEST
+if [[ -f $selected/HEAD ]]; then
+	cd -P $selected
+	branch=$(git worktree list | fzf)
+	read -r first_argument _ <<<"$branch"
+
+    rm -rf ~/public/bookmarks/LATEST
+    ln -s $first_argument ~/public/bookmarks/LATEST
+	cd -P $first_argument
+else
+	if [[ $selected != */bookmarks/LATEST ]]; then
+		rm -rf ~/public/bookmarks/LATEST
+		ln -s $selected ~/public/bookmarks/LATEST
+	fi
+
+	cd -P $selected
 fi
-cd -P $selected
